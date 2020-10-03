e93030522dc42ceb1fc5fc3ee384a3689096f8b8
Sergii Roshchupkin <s.roshchupkin@carfax.eu>
2020-10-03 11:41:09 +0200

    Add solution for remove duplicates
    Add solution for remove duplicates from sorted array for leetcode

--- src/test/java/com/coding/leetcode/easy/DuplicatesSortedArrayTest.java
+++ src/test/java/com/coding/leetcode/easy/DuplicatesSortedArrayTest.java
@@ -1,4 +1,14 @@
-import static org.junit.Assert.*;
+package com.coding.leetcode.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class DuplicatesSortedArrayTest {
-  
+
+    @Test
+    public void shouldRemoveDuplicates() {
+        int[] testArray = new int[]{0, 0, 1, 2, 2, 3, 3, 4, 4, 4, 7, 8, 8};
+        int testArrayLength = DuplicatesSortedArray.removeDuplicates(testArray);
+        Assert.assertEquals(7, testArrayLength);
+    }
 }
